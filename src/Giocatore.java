98edb2db8bdaba4470ae07c63dedfa4f51153ce1
viapiana.21142 <viapiana.riccardo.studente@itispaleocapa.it>
2024-10-11 09:40:28 +0200

    .

---
+++ src/Giocatore.java
@@ -0,0 +1,62 @@
+public class Giocatore {
+    private String nome;
+    private boolean capitano;
+    private int goal;
+    public Giocatore (String nomeGiocatore, boolean isCapitano, int isGoal){
+        setNome(nome);
+        setCapitano(capitano);
+        setGoal(goal);
+    }
+    public Giocatore(String nome){
+        setNome(nome);
+    }
+    public Giocatore(boolean capitano){
+        setCapitano(capitano);
+    }
+    public Giocatore(int goal){
+        setGoal(goal);
+    }
+    public Giocatore(String nome,boolean capitano){
+        setNome(nome);
+        setCapitano(capitano);
+    }
+    public Giocatore(String nome,int goal){
+        setNome(nome);
+        setGoal(goal);
+    }
+    public Giocatore(boolean capitano,int goal){
+        setCapitano(capitano);
+        setGoal(goal);
+    }
+    public Giocatore(){
+        setGoal(goal);
+        setCapitano(capitano);
+        setNome(nome);
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public boolean getCapitano(){
+        return capitano;
+    }
+    public int getGoal(){
+        return goal;
+    }
+
+    public void setNome(String nome) {
+        setNome(nome);
+    }
+
+    public void setCapitano(boolean capitano) {
+        setCapitano(capitano);
+    }
+
+    public void setGoal(int goal) {
+        if(goal<0){
+        }
+        else{
+            setGoal(goal);
+        }
+    }
+}
